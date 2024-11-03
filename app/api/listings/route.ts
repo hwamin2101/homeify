d60f68c736cea6a3c938535720c6cf82547a34a5
hwamin2101 <130414143+hwamin2101@users.noreply.github.com>
2024-11-03 14:01:39 +0000

    Add new feature

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -1,43 +1,46 @@
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prismadb";
 import getCurrentUser from "@/actions/getCurrentUser";
-
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
 
+  console.log(currentUser);
+
   if (!currentUser) {
-    return NextResponse.json({ error: "Người dùng chưa được xác thực." }, { status: 401 });
+    return NextResponse.error();
   }
-
   const body = await request.json();
-  const { title, description, imageSrc, category, roomCount, bathroomCount, guestCount, location, price } = body;
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
 
-  // Validate the request body
-  for (const value of Object.keys(body)) {
+  Object.keys(body).forEach((value: any) => {
     if (!body[value]) {
-      return NextResponse.json({ error: `${value} bắt buộc` }, { status: 400 });
+      NextResponse.error();
     }
-  }
+  });
 
-  try {
-    const listing = await prisma.listing.create({
-      data: {
-        title,
-        description,
-        imageSrc,
-        category,
-        roomCount,
-        bathroomCount,
-        guestCount,
-        locationValue: location?.value || null,
-        price: parseInt(price, 10),
-        userId: currentUser.id,
-      },
-    });
-
-    return NextResponse.json(listing);
-  } catch (error) {
-    console.error("Lỗi khi tạo danh sách:", error);
-    return NextResponse.json({ error: "Không tạo được danh sách" }, { status: 500 });
-  }
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+  return NextResponse.json(listing);
 }
